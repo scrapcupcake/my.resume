dedb1277460d8251ae3076ec5bac09d194646b03
Alisa <alisatana@gmail.com>
2016-08-08 14:37:11 -0700

    Added more structure

---
+++ src/app/rpg-resume/employment.component.ts
@@ -0,0 +1,31 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'employment',
+    template: `<header>
+                <h1 class="dates">{{startDate}} through {{endDate}}</h1>
+                <h1 class="Employer">{{employer}}</h1>
+                <h1 class="Location">{{location}}</h1>
+                </header>
+                <main>
+                <h2 class="JobTitle">{{jobTitle}}</h2>
+                <ng-content></ng-content>
+                </main>
+    `
+})
+export class Employment{
+    @Input('job-title') jobTitle: string;
+    @Input('start-date') startDate: string;
+    @Input('end-date') endDate: string;
+    @Input() employer: string;
+    @Input() location: string;
+
+    ngOnInit(){
+        console.log({
+            jobTitle: this.jobTitle, 
+            startDate: this.startDate,
+            endDate: this.endDate,
+            employer: this.employer,
+            location: this.location });
+    }
+}
